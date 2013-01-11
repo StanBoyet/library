884c5cf0c00af5e7178506d3fb89b99c41bf6d5b
StanBoyet <stanislas.boyet@hei.fr>
2013-01-11 01:16:23 +0100

    Update of book form
    Fixing bug when the input for author's name and firstname were
    displayed as writable instead of beeing readonly

---
+++ README.md
@@ -0,0 +1,7 @@
+== Library - Jelix-based web application
+
+Readme Init
+
+== Online version
+
+Please consult the online version at http://boyet.me/ITI4/ProjetDevWeb/Bibli/www/
--- temp/ProjetDevWeb/www/compiled/locales/modules/jelix~jforms.en_US.UTF-8.php
+++
@@ -1,8 +0,0 @@
-<?php $_loaded= array (
-  'date.year.label' => 'Year',
-  'date.month.label' => 'Month',
-  'date.day.label' => 'Day',
-  'time.hour.label' => 'Hour',
-  'time.minutes.label' => 'Minutes',
-  'time.seconds.label' => 'Seconds',
-) ?>
